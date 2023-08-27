fb04302a1c5167388ab6d12f0ee181e469ffb39e
Kristina Bazaley <atra.nox.ch@gmail.com>
2023-08-27 21:00:49 +0700

    Initial commit

---
+++ src/test/java/ru/netology/spring_01/Spring01ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.spring_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
